d715a185b5af434535b532a2675bc3394d753cc0
Bernard Scheuter <31876955+BernardScheuter@users.noreply.github.com>
2020-02-06 09:31:43 +0100

    Day17

--- html/Form.php
+++ html/Form.php
@@ -15,23 +15,13 @@
 </head>
 <body>
     <header>
-        <img width="100%" src="https://038coder.nl/Images/100daysofcode banner.jpg" alt="Banner #100DaysOfCode" class="left">
-        <div style="text-align:right;">&copy;Karlijn 2020</div>
-        <h2><i>"Debugging my future!"</i></h2>
-        <h6><i>Bernard '20</i></h6>
-        
-        <h3>Status:</h3>
-            <div class="progress" style="background-color: rgb(40, 60, 0);">
-                <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="16" aria-valuemin="0"
-                aria-valuemax="100" style="width:16%">
-                    <div style="color:rgb(40, 60, 0);">16%</div>
-                </div>
-            </div>
+        <img width="100%" src="https://038coder.nl/Images/038CoderBanner.jpeg" alt="Banner #100DaysOfCode" class="left">
+        <iframe src="./Status100DaysOfCode.html" height="200" width="100%" style="border:none;"></iframe>
             <nav>
                 <ul>
                     <li><a href="https://038coder.nl/index.html">Home</a></li>
-                    <li><a href="https://038coder.nl/html/contact.html" target="_blank">Contact</a></li>
-                    <li><a class="selected active" href="https://038coder.nl/html/Form.php" target="_blank">Form</a></li>
+                    <li><a href="https://038coder.nl/html/contact.html">Contact</a></li>
+                    <li><a class="selected active" href="https://038coder.nl/html/Form.php">Form</a></li>
                     <li><a href="https://038coder.nl/days/day1.html">First day</a></li>
                 </ul>
             </nav>
@@ -40,7 +30,7 @@
     <form action="https://038coder.nl/php/verwerk.php" target="_self" method="POST">
        <fieldset> 
         <p>
-        <legend>You can ALMOST leave your feedback here:</legend>
+        <legend>You can leave your feedback here:</legend>
         Your name:<br>
         <input type="text" name="firstname" placeholder="Your name">
         <br>
@@ -54,12 +44,13 @@
         </p>
        </fieldset> 
     </form>
-
+<p>
 <?php
 echo date("Y-M-D_H:i:s");
-echo "<br>Messages:<br>";
-echo readfile("https://038coder.nl/php/message.txt");
-?>    
+echo "<br>Messages:<br><br><br>";
+echo @readfile("https://038coder.nl/php/message.txt");
+?>
+</p>    
 <hr>
     <footer>
                 &copy; Bernard Scheuter 2020 :D<br>
